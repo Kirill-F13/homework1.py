99dc8ce5d48f946192fc3725104acf2f7984f6b5
Kirill-F13 <pergushev.kirill@gmail.com>
2024-07-22 19:55:13 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Kirill'
+print('Name:', name, type(name))
+age = 30
+print('Age:', age, type(age))
+age = age + 7
+print('Nem age:', age, type('new age'))
+is_student = True
+print('Ist Student:', is_student, type(is_student))
