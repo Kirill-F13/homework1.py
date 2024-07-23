302d969c4457e2966f974e755ba84b10debdea0c
Kirill-F13 <pergushev.kirill@gmail.com>
2024-07-23 21:32:29 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_strong = input(' введите ваше имя: ')
+print('количество символов введеного текста: ', len(my_strong))
+print('строка в верхнем регистре:', my_strong.upper())
+print('строка в нижнем регистре:', my_strong.lower())
+print('удалить все пробелы:', my_strong.replace(' ' , ''))
+print('первый символ строки:', my_strong[0])
+print('последний символ строки:', my_strong[-1])
